c69140b13ce13e9d785319873e18e43b489c1aea
Sky <81098308+Ctrl-Sky@users.noreply.github.com>
2025-06-25 22:26:15 -0400

    append 0 to image shift up

--- helpers.py
+++ helpers.py
@@ -1,13 +1,16 @@
 def image_shift_up(image):
     # Expects filename of form IMG_####.JPG and increases #### by one
     try:
-        number = int(image[4:8])
+        number = int(string_number)
     except:
         raise Exception(f"{image}: Incorrect file name. Must match format IMG_####.JPG")
 
     number += 1
-    new_image_name = f"IMG_{str(number)}.JPG"
-    return new_image_name
+    string_number = str(number)
+    missing_digits = 4 - len(string_number)
+    for i in range(missing_digits, -1, -1):
+        string_number = "0" + string_number
 
+    return f"IMG_{string_number}.JPG"
 
-
+print(image_shift_up("IMG_0100.JPG"))
