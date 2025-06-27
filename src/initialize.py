6457970c1aed88dff47393cedf25b19ba41c4f83
Sky <81098308+Ctrl-Sky@users.noreply.github.com>
2025-06-27 02:08:36 -0400

    create initialization step

---
+++ src/initialize.py
@@ -0,0 +1,49 @@
+import csv
+import os
+from helpers import directory_shift_up
+
+def does_path_exist(path):
+    if not os.path.exists(path):
+        raise Exception(f"{path} does not exist")
+
+def initialize_table(table_path):
+    os.makedirs(os.path.dirname(table_path))
+    with open(table_path, 'w') as file:
+        writer = csv.writer(file)
+        writer.writerow(["migration_name", "sd_card_name", "start_dir", "start_image", "start_date", "end_dir", "end_image", "end_date"])
+
+def get_start_values(table_path, sd_card_name):
+    '''
+        Starting from the bottom up in the migration table, look for the most recent migration with the given SD card name
+        If found, set the start values for this migration as the end values of the last migration
+        If not found, set as default start migration values
+    '''
+    with open(table_path, 'r') as file:
+        reversed_reader = reversed(list(csv.reader(file)))
+        for row in reversed_reader:
+            if row[1] == sd_card_name:
+                # Return the end_dir, end_image, end_date
+                return row[5], row[6], row[7]
+        return f"/Volumes/{sd_card_name}/DCIM/100CANON" "IMG_0001.JPG" "N/A"
+    
+def edge_case_9999(start_dir):
+    # For when image is at final name of IMG_9999.JPG, no more space so must shift to new directory
+    new_dir = directory_shift_up(start_dir)
+    os.makedirs(new_dir)
+    return new_dir, "IMG_0001.JPG"
+
+def initialize_repo(sd_card_name, external_hd_name, table_path):
+    # does_path_exist(f"/Volumes/{sd_card_name}")
+    # does_path_exist(f"/Volumes/{external_hd_name}")
+
+    # If table does not exist, create it and add headers
+    if not os.path.exists(table_path):
+        initialize_table(table_path)
+
+    start_dir, start_image, start_date = get_start_values(table_path, sd_card_name)
+
+    # If image at final name
+    if start_image == "IMG_9999.JPG":
+        start_dir, start_image = edge_case_9999(start_dir)
+
+    return start_dir, start_image, start_date
--- src/main.py
+++ src/main.py
@@ -1,22 +1,16 @@
 import argparse
-import os
-from helpers import get_date_taken
+import csv
+from initialize import initialize_repo
+
+# TABLE_PATH = "tables/sd_migration_table.csv"
+TABLE_PATH = "tables/test.csv"
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--sd_card_name", default="SD_CARD_1")
     parser.add_argument("--external_hd_name", default="kl")
+    parser.add_argument("--migration_name", required=False)
     args = parser.parse_args()
 
-    if not os.path.exists(f"/Volumes/{args.sd_card_name}"):
-        raise Exception(f"/Volumes/{args.sd_card_name} does not exist. Ensure SD card is correctly connected")
-    
-    if not os.path.exists(f"/Volumes/{args.external_hd_name}"):
-        raise Exception(f"/Volumes/{args.external_hd_name} does not exist. Ensure HD is correctly connected")
-    
-    if not os.path.exists("tables/sd_migration_table.csv"):
-        os.makedirs("tables")
-        open('tables/sd_migration_table.csv', 'w').close()
-        start_dir = f"/Volumes/{args.sd_card_name}/DCIM/100CANON"
-        start_image = "IMG_0001.JPG"
-        start_time = get_date_taken(f"{start_dir}/{start_image}")
+    print(initialize_repo(args.sd_card_name, args.external_hd_name, TABLE_PATH))
