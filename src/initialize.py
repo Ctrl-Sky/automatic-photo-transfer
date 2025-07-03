671f424e6099998212690316a96d9987c81d137a
Sky <81098308+Ctrl-Sky@users.noreply.github.com>
2025-07-03 00:09:13 -0400

    create convert end to start values function

--- src/initialize.py
+++ src/initialize.py
@@ -1,6 +1,6 @@
 import csv
 import os
-from helpers import directory_shift_up, image_shift_up
+from helpers import directory_shift_up, image_shift_up, get_date_taken
 
 def does_path_exist(path):
     if not os.path.exists(path):
@@ -22,17 +22,16 @@ def initialize_table(table_path):
         writer = csv.writer(file)
         writer.writerow(["migration_name", "sd_card_name", "start_dir", "start_image", "start_date", "end_dir", "end_image", "end_date"])
 
-def get_start_values(table_path, sd_card_path):
+def get_end_values_from_table(table_path, sd_card_path):
     """
-        If the sd card is referenced within the table, get  the most recent photos that were uploaded from that sd card, increment by one set as the the new start point
-        If the sd card is not referenced, return the default start values for the new start point
+        If the sd card is referenced within the table, get  the most recent photos that were uploaded from that sd card
 
         :param table_path: The full path to a csv table
         :type table_path: string
         :param sd_card_path: The full path to a sd card
         :type sd_card_path: string
-        :return: the new file name shifted up by one
-        :rtype: string
+        :return: two values, recently uploaded directory and file name
+        :rtype: string, string
     """
     sd_card_name = os.path.basename(sd_card_path)
     with open(table_path, 'r') as file:
@@ -40,15 +39,35 @@ def get_start_values(table_path, sd_card_path):
         for row in reversed_reader:
             if row[1] == sd_card_name:
                 # Return the end_dir, end_image
-                start_dir, start_image =  row[5], row[6]
+                return row[5], row[6]
 
-                # Shift image up to new values that need to be uploaded
-                start_image = image_shift_up(start_image)
-                if start_image == "Hit Photo Limit":
-                    start_dir, start_image = edge_case_9999(start_dir)
-                return start_dir, start_image
+        return "NOT", "FOUND"
+    
+def convert_end_to_start_values(end_dir, end_image, sd_card_path):
+    """
+        Converts the last uploaded directory and image values into the starting directory, image, and date for the next migration.
 
-        return f"{sd_card_path}/DCIM/100CANON", "IMG_0001.JPG"
+        If no previous migration is found, defaults to the first directory and image on the SD card. Otherwise, shifts to the next image (or directory if at the image limit).
+
+        :param end_dir: The directory of the last uploaded image
+        :type end_dir: string
+        :param end_image: The file name of the last uploaded image
+        :type end_image: string
+        :param sd_card_path: The full path to the SD card
+        :type sd_card_path: string
+        :return: three values, the start directory, image, and date for the next migration
+        :rtype: string, string, string
+    """
+    if end_dir == "NOT" and end_image == "FOUND":
+        start_dir = f"{sd_card_path}/DCIM/100CANON"
+        start_image = "IMG_0001.JPG"
+        start_date = get_date_taken(f"{start_dir}/{start_image}")
+    else:
+        start_image = image_shift_up(start_image)
+        if start_image == "Hit Photo Limit":
+            start_dir, start_image = edge_case_9999(start_dir)
+        start_date = get_date_taken(f"{start_dir}/{start_image}")
+    return start_dir, start_image, start_date
     
 def edge_case_9999(start_dir):
     # For when image is at final name of IMG_9999.JPG, no more space so must shift to new directory
@@ -77,5 +96,6 @@ def initialize_repo(sd_card_path, external_hd_path, table_path):
     # If table does not exist, create it and add headers
     if not os.path.exists(table_path):
         initialize_table(table_path)
-
-    return get_start_values(table_path, sd_card_path)
+    
+    end_dir, end_image = get_end_values_from_table(table_path, sd_card_path)
+    return convert_end_to_start_values(end_dir, end_image, sd_card_path)
