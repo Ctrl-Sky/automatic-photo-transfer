5f92cf595c272953fb595da4d92e55016d7c36c4
Sky <81098308+Ctrl-Sky@users.noreply.github.com>
2025-07-02 23:19:46 -0400

    add reST docstring

--- src/helpers.py
+++ src/helpers.py
@@ -3,7 +3,15 @@ from datetime import datetime
 from PIL import Image
 
 def image_shift_up(image):
-    # Expects filename of form IMG_####.JPG and increases #### by one
+    """
+        Takes an image of form IMG_####.JPG and increases the value by one. 
+        For example, given IMG_0001.JPG, it will return IMG_0002.JPG
+
+        :param image: the file name of the image
+        :type image: string
+        :return: the new file name shifted up by one
+        :rtype: string
+    """
     try:
         number = int(image[4:8])
     except:
@@ -24,7 +32,15 @@ def image_shift_up(image):
     return f"IMG_{string_number}.JPG"
 
 def image_shift_down(image):
-    # Expects filename of form IMG_####.JPG and increases #### by one
+    """
+        Takes an image of form IMG_####.JPG and decreases the value by one. 
+        For example, given IMG_0002.JPG, it will return IMG_0001.JPG
+        
+        :param image: the file name of the image
+        :type image: string
+        :return: the new file name shifted down by one
+        :rtype: string
+    """
     try:
         number = int(image[4:8])
     except:
@@ -45,7 +61,15 @@ def image_shift_down(image):
     return f"IMG_{string_number}.JPG"
 
 def directory_shift_down(dir):
-    # Expects directory in the form .../.../.../1[0-9][0-9]CANON
+    """
+        Takes a directory of form path/to/image/###CANON and decreases the value by one. 
+        For example, given /Volumes/SD_CARD_1/DCIM/101CANON, it will return /Volumes/SD_CARD_1/DCIM/100CANON
+        
+        :param dir: the directory containing the images
+        :type dir: string
+        :return: the new directory shifted down by one
+        :rtype: string
+    """
     try:
         dir_name = os.path.basename(os.path.normpath(dir))
         number = int(dir_name[:3])
@@ -64,7 +88,15 @@ def directory_shift_down(dir):
     return f"{parent_dir}/{new_dir_name}"
 
 def directory_shift_up(dir):
-    # Expects directory in the form .../.../.../1[0-9][0-9]CANON
+    """
+        Takes a directory of form path/to/image/###CANON and increases the value by one. 
+        For example, given /Volumes/SD_CARD_1/DCIM/100CANON, it will return /Volumes/SD_CARD_1/DCIM/101CANON
+        
+        :param dir: the directory containing the images
+        :type dir: string
+        :return: the new directory shifted up by one
+        :rtype: string
+    """
     try:
         dir_name = os.path.basename(os.path.normpath(dir))
         number = int(dir_name[:3])
@@ -79,13 +111,30 @@ def directory_shift_up(dir):
     return f"{parent_dir}/{new_dir_name}"
 
 def get_date_taken(image_path):
+    """
+        Takes a path to an image, reads the EXIF and returns the date the photo was taken
+        
+        :param image_path: The full path including file name of the image
+        :type image_path: string
+        :return: the date the photo was taken
+        :rtype: string
+    """
     exif = Image.open(image_path)._getexif()
     if not exif:
         return "Image does not have EXIF data"
     return exif[36867]
 
 def convert_to_month_year(exif_date, include_date=False):
-    # Expects exif_date to be in form of %Y:%m:%d %H:%M:%S
+    """
+        Convert an exif date in the form YYYY:MM:DD HH:MM:SS into a pretty print of just month-year or month-date-year
+        
+        :param exif_date: A date in pulled from the EXIF
+        :type exif_date: string
+        :param include_date: Whether or not to return the date, default to False
+        :type include_date: bool, optional
+        :return: An easier to read string of the date
+        :rtype: string
+    """
     date = exif_date.split(" ")[0]
     datetime_obj = datetime.strptime(date, "%Y:%m:%d")
     
