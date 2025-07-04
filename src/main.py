334b06f7c965fff360cda82680dd3464abf7a6a1
Sky <81098308+Ctrl-Sky@users.noreply.github.com>
2025-07-03 21:21:44 -0400

    edit main.py to reflect new changes

--- src/main.py
+++ src/main.py
@@ -8,9 +8,9 @@ TABLE_PATH = "tables/test.csv"
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--sd_card_name", default="SD_CARD_1")
-    parser.add_argument("--external_hd_name", default="kl")
+    parser.add_argument("--sd_card_name", default="/Volumes/SD_CARD_1")
+    parser.add_argument("--external_hd_name", default="/Volumes/kl")
     parser.add_argument("--migration_name", required=False)
     args = parser.parse_args()
 
-    start_dir, start_image = initialize_repo(args.sd_card_name, args.external_hd_name, TABLE_PATH)
+    start_dir, start_image, start_date = initialize_repo(args.sd_card_name, args.external_hd_name, TABLE_PATH)
